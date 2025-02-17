61ef92602b4c1d26ddf2831b32c9b62e7baa48e3
poojabhatia1505 <yoyopooletsdoit@gmail.com>
2025-02-17 20:50:31 +0000

    datatypes

---
+++ 01_basic/conversion/conversion.js
@@ -0,0 +1,22 @@
+// let score="pooja"
+// console.log(typeof(score))
+
+// let valueInNumber=Number(score);
+// console.log(typeof(valueInNumber))
+// console.log(valueInNumber)
+
+//"33"=33
+//"33abc"=nan
+//null=0
+//true=1;false=0;
+
+let isLoggedIn="pooja"
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+//0=false 1=true
+//""=false
+//null=false
+//"any string"=true
+
+//for converting into string use //'String'// func
